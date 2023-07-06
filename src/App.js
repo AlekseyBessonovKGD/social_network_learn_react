3b5e943c2f7dbbbb65f97359935f496fe15350ba
Aleksey_Bessonov <Lexx-88@mail.ru>
2023-07-06 21:22:11 +0200

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import NavBar from "./components/NavBar/NavBar";
+import Profile from "./components/Profile/Profile";
+import Messages from "./components/Messages/Messages";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Friends from "./components/Friends/Friends";
+import News from "./components/News/News";
+import Settings_list from "./components/Settings_list/Settings_list";
+import Games from "./components/Games/Games";
+import Music from "./components/Music/Music";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="my-app">
+        <Header />;
+        <NavBar />;
+        <div className="color-them">
+          <Routes>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/messages" element={<Messages />} />
+            <Route path="/friends" element={<Friends />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/settings" element={<Settings_list />} />
+            <Route path="/games" element={<Games />} />
+            <Route path="/music" element={<Music />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
